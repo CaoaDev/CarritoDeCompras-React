a1de042e2afe42ba96e052e63b074bb9f9481ab0
CaoaDev <caoa_sinos9@outlook.es>
2022-07-26 20:58:31 -0500

    update

---
+++ src/data/smartPhones.jsx
@@ -0,0 +1,49 @@
+export const smarts = [
+    {
+        'id': 'GALAXY A03',
+        'Marca':'Samsung',
+        'Camara':'Delantera 5MP y trasera 2MP',
+        'Almacenamiento':'128GB',
+        'Ram':'4GB',
+        'Pantalla':'6.5"'
+    },
+    {
+        'id': 'GALAXY A13',
+        'Marca':'Samsung',
+        'Camara':'Delantera 8MP y trasera 50MP',
+        'Almacenamiento':'128GB',
+        'Ram':'4GB',
+        'Pantalla':'6.6"'
+    },
+    {
+        'id': 'HUAWEI Y6P',
+        'Marca':'Huawei',
+        'Camara':'Delantera 8MP y trasera 13 + 5 + 2MP',
+        'Almacenamiento':'64GB',
+        'Ram':'3GB',
+        'Pantalla':'6.3"'
+    },
+    {
+        'id': 'HUAWEI Y9 PRIME',
+        'Marca':'Huawei',
+        'Camara':'Delantera 16MP y trasera 16 + 8 + 2MP',
+        'Almacenamiento':'128GB',
+        'Ram':'4GB',
+        'Pantalla':'6.59"'
+    },
+    {
+        'id': 'MOTO E6 PLUS',
+        'Marca':'Motorola',
+        'Camara':'Delantera 8MP y trasera 13 + 2MP',
+        'Almacenamiento':'32GB expandible hasta 512GB',
+        'Ram':'2GB',
+        'Pantalla':'6.2"'
+    },
+    {
+        'id': 'MOTO G60',
+        'Marca':'Motorola',
+        'Camara':'Delantera 5MP y trasera 2MP',
+        'Almacenamiento':'128GB',
+        'Ram':'6GB',
+        'Pantalla':'6.8"'
+    }
